05227ec5d42e4dd650b583b5fa98b65b5ba37d6b
gaBriel nesTor bruFau <70489784+qukiiignb@users.noreply.github.com>
2023-07-02 08:51:34 -0300

    Update equality.c

--- UNLOCK-CHALLENGES/1/a/equality.c
+++ UNLOCK-CHALLENGES/1/a/equality.c
@@ -1,5 +1,21 @@
-# import <iostream>
+#include <stdio.h>
 
-func main(){
+int main(){
+ int a;
+ int b;
+ int c = b;
+
+ // print -
+ printf("enter number a:");
+ // scanf - read input salve in variable
+ scanf("%d",&a);
+ printf("enter number b:");
+ scanf("%d",&a);
+
+ if (a = a){printf("reflexive")}else{printf("no reflexive")};
+ if (a = b && b = a){printf("symmetric")else{printf("no symetric")};
+ if (a=b&&b=c&&c=a){printf("transitivity")else{printf("no transivity")};
+ if (a*2=b*2){printf("the number are equaliy great!")else{printf("something was wrong")};
+ return 0;
  
 }
