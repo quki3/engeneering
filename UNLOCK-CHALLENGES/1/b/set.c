210ffca6e2811e9bf40d55f6db58a399f0ff2025
gaBriel nesTor bruFau <70489784+qukiiignb@users.noreply.github.com>
2023-07-03 08:05:15 -0300

    Create set.c

---
+++ UNLOCK-CHALLENGES/1/b/set.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// struct - allows you to group multiple variables of different data types together under a single name.
+struct Person {
+    char name[20];
+    int age;
+};
+
+int main() {
+    struct Person person1;
+
+    // strcpy - is a standard library function that is used to copy a string from one character array (source) to another character array (destination). The name strcpy stands for "string copy".
+    strcpy(person1.name, "John");
+    person1.age = 30;
+
+    // 
+    printf("Name: %s\n", person1.name);
+    printf("Age: %d\n", person1.age);
+
+    return 0;
+}
