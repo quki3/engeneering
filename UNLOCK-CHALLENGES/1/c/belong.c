3a447fe54a1b77a656c1f0cfb2e31c6dd2cd6165
Quki <70489784+quki3@users.noreply.github.com>
2023-07-04 15:44:44 -0300

    Create belong.c

---
+++ UNLOCK-CHALLENGES/1/c/belong.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define MAX_LENGTH 50
+#define ARRAY_SIZE 5
+
+int main(){
+char abc[ARRAY_SIZE][MAX_LENGTH]={"a","b","c","d","e"};
+  //white a number to belong
+
+  // compare with abc
+
+  // print the answer
+
+return 0;
+}
