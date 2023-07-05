f583ec9eea873141b41c023ea8d47976968c8b64
Quki <70489784+quki3@users.noreply.github.com>
2023-07-05 16:39:18 -0300

    Update belong.c

--- UNLOCK-CHALLENGES/1/c/belong.c
+++ UNLOCK-CHALLENGES/1/c/belong.c
@@ -6,9 +6,14 @@
 int main(){
 char abc[ARRAY_SIZE][MAX_LENGTH]={"a","b","c","d","e"};
   //white a number to belong
-
+  int x
+  scanf("%d",&x);
   // compare with abc
-
+  for (i=0;i<=sinzeof(abc);i++){
+    if (x=abc[i]){
+      printf("%d belong abc",x)
+        }
+  }
   // print the answer
 
 return 0;
