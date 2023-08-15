79c2c62c400dc455394030639a5e3efcd5933131
Quki <70489784+quki3@users.noreply.github.com>
2023-08-15 12:05:04 -0300

    Update 4.go

--- Timeline/mathematics/math-1/uplock-challenges/4.go
+++ Timeline/mathematics/math-1/uplock-challenges/4.go
@@ -1,9 +1,13 @@
 /* TEORY
 
 square root of x = [2]root{x}
+periodic = ~
 
 [2]root{25} = 25^1/2 = [2]root{ 5 * 5 ] = [2]root{5} * [2]root{5} = 5
 5 = N(natural number)
+
+33/9 = (27/9)+(6/9) = 3 + 6/9 = 3 + { x = 6/9 } = 3 + { 0,x = 60/9 } = 3 + { 0,x = (54/9)+(6/9) } = 3 + {0,6x = 6/9 } = 3+0,6~
+
 */
 package main
 
