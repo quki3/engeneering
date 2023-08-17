c47c1ddf4727775d8bb8856c16bc9db85c34932b
Quki <70489784+quki3@users.noreply.github.com>
2023-08-17 14:34:57 -0300

    Update 6.go

--- Timeline/mathematics/math-1/uplock-challenges/6.go
+++ Timeline/mathematics/math-1/uplock-challenges/6.go
@@ -16,3 +16,27 @@ teory
 6+ (1*(-24))
 6-24
 -18
+***/
+
+package main
+import (
+  "fmt"
+)
+func main () {
+  err , result := res()
+  if err != err {
+    fmt.Println("%d",err)
+  }
+  fmt.Println("%d",result)
+}
+func sum (number ...int) error int {
+  var res int
+  for err, cal = range number {
+    res += cal
+  }
+  if err != nil {
+    return error
+  }
+  return res
+}
+  
