f1baf07539d9ea399c1cf87adfcd99e44acf31ac
Quki <70489784+quki3@users.noreply.github.com>
2023-08-17 15:13:45 -0300

    Create 7.go

---
+++ Timeline/mathematics/math-1/uplock-challenges/7.go
@@ -0,0 +1,23 @@
+/*** 
+teory
+5 + x 
+4/5(pi)*r^3
+2\/2m^2n^3\
+***/
+package main
+import (
+  "fmt"
+)
+
+func main () {
+  const (
+    pi = 3.14
+    five = 5
+    fourOnfive = 4/5
+    two = 2
+  )
+  x+5(x)
+}
+func x+5 (x int) int {
+    return  five*x
+}
