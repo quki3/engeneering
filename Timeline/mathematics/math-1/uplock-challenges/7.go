49e4c3a6910f59af7abe41340df7225ad03172af
Quki <70489784+quki3@users.noreply.github.com>
2023-08-17 15:53:00 -0300

    Update 7.go

--- Timeline/mathematics/math-1/uplock-challenges/7.go
+++ Timeline/mathematics/math-1/uplock-challenges/7.go
@@ -15,8 +15,11 @@ func main () {
     five = 5
     fourOnfive = 4/5
     two = 2
+    x = [5]array{0,1,5,0.3}
   )
-  x+5(x)
+  for err , res = range x {
+    x+5(res)
+  }
 }
 func x+5 (x int) int {
     return  five*x
