a5a10c408d3d6763672da70ee176c67eb32ab8b4
Quki <70489784+quki3@users.noreply.github.com>
2023-08-17 16:18:44 -0300

    Create 2.go

---
+++ Timeline/physics/physics-I/challenges/2.go
@@ -0,0 +1,11 @@
+/** 
+teory 
+you need to convert a unit 
+you need to know the convertion factor
+
+how much secound within in 1 hours
+you neet to know how this unit work
+
+1hs/1 * 60min/1hs = 60min * 60sec/1min = 3600sec * 60nsec/1sec
+
+**/
