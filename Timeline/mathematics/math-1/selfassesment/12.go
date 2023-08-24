5989d1e69327eeb6900d210d7f307057127d193c
Quki <70489784+quki3@users.noreply.github.com>
2023-08-24 06:15:54 -0300

    Create 12.go

---
+++ Timeline/mathematics/math-1/selfassesment/12.go
@@ -0,0 +1,12 @@
+/*** 
+teory
+leading 1
+x^2 + ( GCF1 + GCF2 )x  + GCF = (x + GCF1)*(x + GCF2)
+
+x^3 + a = (x + a) ( x^2 -ax + a^2)
+x^3 - a = (x - a) ( x^2 + ax +a^2)
+
+ax^{c/d} + bx^{e/f} = gcfx^{minor exponent} ( remeinder^{remeinder} + remeinder^{remeinder} )
+
+
+***/
